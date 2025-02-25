66da31ce480f7f9b9f23cdef174a6c69d0571974
Ekaterina Shchekotikhina <asti.katerina@gmail.com>
2025-02-25 22:05:28 +0100

    refactor: project setup with Pinia and Vuetify integration

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,30 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
+import piniaPersist from 'pinia-plugin-persistedstate'
+import 'vuetify/styles' 
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+const pinia = createPinia()
+pinia.use(piniaPersist)
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      },
+    },
+  })
+
+app.use(pinia).use(vuetify)
+
+app.mount('#app')
