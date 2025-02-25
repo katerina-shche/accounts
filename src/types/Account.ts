f9374592e5a9ff95945d7eb541c8856e0f99bdd7
Ekaterina Shchekotikhina <asti.katerina@gmail.com>
2025-02-25 22:07:43 +0100

    feat: add Account and Mark interfaces with AccountsState management

---
+++ src/types/Account.ts
@@ -0,0 +1,8 @@
+import Mark from '@/types/Mark'
+
+export default interface Account {
+    marks: Mark[],
+    recordType: 'Локальная' | 'LDAP',
+    login: string,
+    password: string | null
+  }
