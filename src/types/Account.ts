abc6038db604df8364fff06b9812bdcbc626b9be
Ekaterina Shchekotikhina <asti.katerina@gmail.com>
2025-02-25 23:03:43 +0100

    feat: add id field to Account interface for unique identification

--- src/types/Account.ts
+++ src/types/Account.ts
@@ -4,5 +4,6 @@ export default interface Account {
     marks: Mark[],
     recordType: 'Локальная' | 'LDAP',
     login: string,
-    password: string | null
+    password: string | null,
+    id: string,
   }
