8ca684dbdb135734ae2e1ba098a6b1336efee52d
Ekaterina Shchekotikhina <asti.katerina@gmail.com>
2025-02-26 21:29:51 +0100

    refactor: clean up accountsStore by removing unused interfaces and getters

--- src/stores/accountsStore.ts
+++ src/stores/accountsStore.ts
@@ -3,33 +3,11 @@ import Account from '@/types/Account';
 import AccountsState from '@/types/AccountsState';
 import { nanoid } from 'nanoid';
 
-// interface Mark {
-//   text: string,
-// }
-
-// interface Account {
-//   marks: Mark[],
-//   recordType: 'Локальная' | 'LDAP',
-//   login: string,
-//   password: string | null,
-//   id: string,
-// }
-// interface AccountsState {
-//   accounts: Account[];
-// }
-
 export const useAccountsStore = defineStore('accountsStore', {
   state: (): AccountsState => ({
     accounts: [] as Account[],
   }),
-  getters: {
-    // просто для теста: беру аккаунт по индексу
-    getAccountByIndex: (state) => (index: number): Account => {
-      return state.accounts[index];
-    },
-  },
   actions: {
-    // добавляю новый пустой аккаунт
     addEmptyAccount(): void {
       this.accounts.push({
         marks: [],
@@ -39,7 +17,6 @@ export const useAccountsStore = defineStore('accountsStore', {
         id: nanoid(),
       });
     },
-    // удаляю аккаунт по логину
     deleteAccountById(id: string): void {
       this.accounts = this.accounts.filter((acc) => acc.id !== id);
     },
